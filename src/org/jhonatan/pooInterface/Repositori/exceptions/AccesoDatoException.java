eb9a30e812f272cf77535f5c4d883ec272a5d32b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-02 10:36:04 -0500

    class AccesoDatoException

---
+++ src/org/jhonatan/pooInterface/Repositori/exceptions/AccesoDatoException.java
@@ -0,0 +1,7 @@
+package org.jhonatan.pooInterface.Repositori.exceptions;
+
+public class AccesoDatoException extends Exception {
+    public AccesoDatoException(String mensaje) {
+        super(mensaje);
+    }
+}
