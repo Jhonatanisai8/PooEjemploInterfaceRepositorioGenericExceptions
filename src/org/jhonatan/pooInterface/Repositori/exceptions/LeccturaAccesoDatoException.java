dfb7b77e6abeed36c777cdaced652fc286b71472
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-02 10:37:03 -0500

    class LeccturaAccesoDatoException

---
+++ src/org/jhonatan/pooInterface/Repositori/exceptions/LeccturaAccesoDatoException.java
@@ -0,0 +1,9 @@
+package org.jhonatan.pooInterface.Repositori.exceptions;
+
+public class LeccturaAccesoDatoException extends AccesoDatoException {
+
+    public LeccturaAccesoDatoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
